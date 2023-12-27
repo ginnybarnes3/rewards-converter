701b2951f52da99ea0c75d1ee663fc7e8ae5b6d6
ginnybarnes3 <gcbarnes@usc.edu>
2023-12-26 20:53:17 -0800

    Add Unit Tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
+    static final double conversionRate = 0.0035;
+
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +22,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int expectedMilesValue = (int) Math.round(cashValue/conversionRate);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 1000;
+        double expectedCashValue = milesValue*(conversionRate);
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
